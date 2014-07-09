50006b5509d13d205d57645ca5790bf6a6af9f9c
ztylff7 <Legend1883@Tianyis-MacBook-Pro.local>
2014-07-09 14:56:25 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Resources/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Tianyi Zhang on 09/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Resources/WaitingPenguin.m
@@ -0,0 +1,30 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by Tianyi Zhang on 09/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+- (void)didLoadFromCCB
+{
+    // generate a random number between 0.0 and 2.0
+    float delay = (arc4random() % 2000) / 1000.f;
+    // call method to start animation after random delay
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
+}
+
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
+
+
+@end
