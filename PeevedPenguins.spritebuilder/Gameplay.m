e6ad4709db11056ec726e7094a93347fc34c4dbb
ztylff7 <Legend1883@Tianyis-MacBook-Pro.local>
2014-07-09 02:24:55 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -67,26 +67,7 @@
     }
 }
 
-- (void)launchPenguin {
-    //loads the Penguin.ccb we have set up in Spritebuilder
-    CCNode* penguin = [CCBReader load:@"Penguin"];
-    // position the penguin at the bowl of the catapult
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
-    
-    // add the penguin to the physicsNode of this scene (because it has physics enabled)
-    [_physicsNode addChild:penguin];
-    
-    // manually create & apply a force to launch the penguin
-    CGPoint launchDirection = ccp(1,0);
-    CGPoint force = ccpMult(launchDirection, 8000);
-    [penguin.physicsBody applyForce:force];
-    
-    // ensure followed object is in visible when starting
-    self.position = ccp(0,0);
-    CCActionFollow *follow = [CCActionFollow actionWithTarget: penguin worldBoundary:self.boundingBox];
-    [_contentNode runAction: follow];
-    
-}
+
 
 - (void)retry {
     //reload this level
